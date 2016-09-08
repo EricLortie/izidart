a386148086f109788a16d6163b0ee7154206395b
Eric Lortie <e@ericlortie.com>
2016-09-07 21:14:49 -0300

    Removing the old javascript for geolocation.

--- post_item.php
+++ post_item.php
@@ -173,38 +173,6 @@ if(is_user_logged_in()){
                             </div>
                           </div>
                         </div>
-                        <script>
-                          jQuery(window).ready(function(){
-                              initiate_geolocation();
-                          });
-
-                          function initiate_geolocation() {
-                              navigator.geolocation.getCurrentPosition(handle_geolocation_query,handle_errors);
-                          }
-
-                          function handle_errors(error)
-                          {
-                              switch(error.code)
-                              {
-                                  case error.PERMISSION_DENIED: alert("user did not share geolocation data");
-                                  break;
-
-                                  case error.POSITION_UNAVAILABLE: alert("could not detect current position");
-                                  break;
-
-                                  case error.TIMEOUT: alert("retrieving position timed out");
-                                  break;
-
-                                  default: alert("unknown error");
-                                  break;
-                              }
-                          }
-
-                          function handle_geolocation_query(position){
-                              alert('Lat: ' + position.coords.latitude +
-                                    ' Lon: ' + position.coords.longitude);
-                          }
-                      </script>
 
 
                         <div class="field large-12 small-12 columns">
